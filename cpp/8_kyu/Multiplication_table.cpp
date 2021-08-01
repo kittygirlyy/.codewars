ca2251f277072ea5d3da57270e514e1c1b426d03
n3k0girl <87582918+n3k0girl@users.noreply.github.com>
2021-08-01 18:44:20 +0200

    Codewars C++

---
+++ cpp/8_kyu/Multiplication_table.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+
+std::string multi_table(int Number)
+{
+  std::string Table{};
+  int Response{};
+    for(int i=1;i<=10;i++)
+      {
+      Response = Number * i;
+      std::string sNumber = std::to_string(Number);
+      std::string sResponse = std::to_string(Response);
+      std::string sInt = std::to_string(i);
+      if (i == 10)
+        {
+        Table += sInt + " * " + sNumber + " = " + sResponse;
+        }
+      else
+        {
+        Table += sInt + " * " + sNumber + " = " + sResponse + "\n";
+        }
+      }
+    return Table;
+}
